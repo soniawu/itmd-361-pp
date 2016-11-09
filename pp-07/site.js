c6d26f6e5ab9db96a99e91deb9b5c08186b0e53c
Sonia Wu <swu52@hawk.iit.edu>
2016-11-08 19:45:28 -0600

    Solve problem 1 and 2

--- pp-07/site.js
+++ pp-07/site.js
@@ -3,15 +3,24 @@
   outputs the correct value for x (5).
 */
 
-var x = 5;
+/* Make the old codes to self invoking function will
+    block variables and functions being access via
+    global objects. This solve problem 2.
+*/
+(function() {
+  var x = 5;
+  function double(num) {
+    /*  add var in front of x to define it as local variable 
+        to solve problem 1.
+    */
+    var x = num * 2;
+    return x;
+  }
+  double(6);
+  console.log('The value of x is:', x, 'It should be 5.');
+})()
 
-function double(num) {
-  x = num * 2;
-  return x;
-}
 
-double(6);
-console.log('The value of x is:', x, 'It should be 5.');
 
 /*
   2. Rewrite the corrected JavaScript above as function that
